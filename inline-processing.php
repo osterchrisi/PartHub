2ca25adbb9b240f3833373a4dd5da6c0f7177919
chrisi <chrisi@mobstationT.local>
2023-03-06 23:48:06 +0100

    in-line table data selection

--- inline-processing.php
+++ inline-processing.php
@@ -1,32 +1,29 @@
 <script>
 $(document).ready(function() {
-  $('tbody tr').click(function() {
-    var id = $(this).data('id');
-    var number = $(this).find('.editable').text();
-    var formHtml = '<form><input type="number" name="number" value="' + number + '"> ';
-    formHtml += '<input type="hidden" name="id" value="' + id + '"> ';
-    formHtml += '<input type="submit" value="OK"></form>';
-
-    // Replace the current cell contents with the form
-    $(this).find('.editable').html(formHtml);
-  });
-
-  $('tbody').on('submit', 'form', function(e) {
-    e.preventDefault();
-
-    // Get the form data
-    var formData = $(this).serialize();
-
-    // Submit the form data via AJAX to update the database
-    $.ajax({
-      type: 'POST',
-      url: 'update_number.php',
-      data: formData,
-      success: function(response) {
-        // Replace the form with the new number
-        var newNumber = $(response).find('.editable').html();
-        $(this).parent().html(newNumber);
-      }
+  $('tbody td.editable').click(function() {
+    var cell = $(this);
+    var currentValue = cell.text();
+    var input = $('<input type="number">').val(currentValue);
+    cell.empty().append(input);
+    input.focus();
+    input.blur(function() {
+      var newValue = input.val();
+      cell.text(newValue);
+      var id = cell.closest('tr').data('id');
+      $.ajax({
+        type: 'POST',
+        url: 'update.php',
+        data: {
+          id: id,
+          number: newValue
+        },
+        success: function(data) {
+          console.log('Data updated successfully');
+        },
+        error: function(xhr, status, error) {
+          console.error('Error updating data');
+        }
+      });
     });
   });
 });
