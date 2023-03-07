71119351007d26432881c5001275f99ea3c4e0db
chrisi <chrisi@mobstationT.local>
2023-03-07 17:22:32 +0100

    Fixed bug in viewport and updated screenshots

---
+++ categories.php
@@ -0,0 +1,20 @@
+<?php
+  $title = 'Parts';
+  require_once('head.html');
+  include 'config/credentials.php'; 
+  include 'inline-processing.php';
+  include 'lib/SQL.php';
+  include 'lib/forms.php';
+  $table_name = "part_categories";
+  
+?>
+
+<style>
+    .fixed-bottom {
+  height: 100%; /* Set the height as needed */
+}
+</style>
+<div class="container-fluid">
+<?php require_once('navbar.php');?>
+<br>
+<h4>Categories</h4>
