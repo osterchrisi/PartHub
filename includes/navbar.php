819d12410d58aadcac88c08fcd7ac66142fe27c6
chrisi <chrisi@mobstationT.local>
2023-03-13 14:53:52 +0100

    Added frontpage, fixed navbar links

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">PartHub</a>
+    <a class="navbar-brand" href="/PartHub/index.php">PartHub</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/inventory.php') ? 'active' : ''; ?>" href="inventory.php">Parts</a>
+          <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/inventory.php') ? 'active' : ''; ?>" href="/PartHub/pages/inventory.php">Parts</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/bom-create.php' OR $_SERVER['PHP_SELF'] == '/PartHub/pages/bom-build.php' OR $_SERVER['PHP_SELF'] == '/PartHub/pages/bom-search.php') ? 'active' : ''; ?>" href="../pages/bom-search.php" role="button" data-bs-toggle="dropdown">BOMS</a>
+          <a class="nav-link dropdown-toggle <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/bom-create.php' OR $_SERVER['PHP_SELF'] == '/PartHub/pages/bom-build.php' OR $_SERVER['PHP_SELF'] == '/PartHub/pages/bom-search.php') ? 'active' : ''; ?>" href="/PartHub/pages//bom-search.php" role="button" data-bs-toggle="dropdown">BOMS</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../pages/bom-create.php">Create BOM</a></li>
-            <li><a class="dropdown-item" href="../pages/bom-build.php">Build BOM</a></li>
-            <li><a class="dropdown-item" href="../pages/bom-search.php">Show BOM</a></li>
+            <li><a class="dropdown-item" href="/PartHub/pages/bom-create.php">Create BOM</a></li>
+            <li><a class="dropdown-item" href="/PartHub/pages/bom-build.php">Build BOM</a></li>
+            <li><a class="dropdown-item" href="/PartHub/pages/bom-search.php">Show BOM</a></li>
           </ul>
         </li>
         <!-- <li class="nav-item dropdown">
@@ -25,19 +25,19 @@
           </ul>
         </li> -->
         <li class="nav-item">
-          <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/locations.php') ? 'active' : ''; ?>" href="../pages/locations.php">Storage Locations</a>
+          <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/locations.php') ? 'active' : ''; ?>" href="/PartHub/pages/locations.php">Storage Locations</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/categories.php') ? 'active' : ''; ?>" href="../pages/categories.php">Categories</a>
+          <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/categories.php') ? 'active' : ''; ?>" href="/PartHub/pages/categories.php">Categories</a>
         </li>
         <div class="d-flex flex-row-reverse">
           <li class="nav-item">
-            <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/suppliers.php') ? 'active' : ''; ?>" href="../pages/suppliers.php">Suppliers</a>
+            <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/suppliers.php') ? 'active' : ''; ?>" href="/PartHub/pages/suppliers.php">Suppliers</a>
           </li>
         </div>
         <div class="d-flex flex-row-reverse">
           <li class="nav-item">
-            <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/footprints.php') ? 'active' : ''; ?>" href="../pages/footprints.php">Footprints</a>
+            <a class="nav-link <?php echo ($_SERVER['PHP_SELF'] == '/PartHub/pages/footprints.php') ? 'active' : ''; ?>" href="/PartHub/pages/footprints.php">Footprints</a>
           </li>
         </div>
       </ul>
--- index.php
+++ index.php
@@ -1,54 +1,46 @@
 <?php
 $title = 'Start';
-include 'head.html';
-include 'navbar.php'; ?>
+include 'includes/head.html';
+include 'includes/navbar.php'; ?>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
+<!-- <style>
+  table {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style> -->
 
-<div class="container-fluid">
-    <!-- <div class="d-flex align-items-center justify-content-center" style="height:100vh">
-        <div class="row">
-            <div class="col-6">
-                Parts
-            </div>
-            <div class="col-6">
-                BOMS
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-6">
-                Storage Locations
-            </div>
-            <div class="col-6">
-                Footprints
-            </div>
-        </div>
-    </div> -->
-    <div class="d-flex align-items-center justify-content-center" style="height:100vh">
-    <div class="text-center">
-      <a href="#">
-        <i class="fas fa-home fa-5x"></i>
-      </a>
-      <a href="#">
-        <i class="fas fa-envelope fa-5x"></i>
-      </a><br>
-      <a href="#">
-        <i class="fas fa-phone fa-5x"></i>
-      </a>
-      <a href="#">
-        <i class="fas fa-info fa-5x"></i>
-      </a>
+<div class="container-fluid vh-100 d-flex flex-column">
+  <div class="row d-flex flex-grow-1">
+    <div class="col-md-6 flex-grow-1 mx-auto my-auto">
+      <table class="table table-bordered text-center mx-auto w-auto">
+        <tr>
+          <td colspan="3">Welcome to PartHub Live Demo!<br><br>
+          You can use any aspect of this demo but the database will reset every full hour, so all your changes will be lost</td>
+        </tr>
+        <tr>
+          <td><a href="#"><i class="fa fa-home"></i><br>Parts</a></td>
+          <td><a href="#"><i class="fa fa-home"></i><br>BOMs</a></td>
+          <td><a href="#"><i class="fa fa-home"></i><br>Storage Locations</a></td>
+        </tr>
+        <tr>
+          <td><a href="#"><i class="fa fa-home"></i><br>Categories</a></td>
+          <td><a href="#"><i class="fa fa-home"></i><br>Suppliers</a></td>
+          <td><a href="#"><i class="fa fa-home"></i><br>Footprints</a></td>
+        </tr>
+      </table>
     </div>
   </div>
-
 </div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+  integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
 
-<!-- <div class="d-flex align-items-center justify-content-center" style="height:100vh">
-    <div class="text-center">
-      <a href="#" class="btn btn-primary btn-lg">Link 1</a>
-      <a href="#" class="btn btn-primary btn-lg">Link 2</a><br><br>
-      <a href="#" class="btn btn-primary btn-lg">Link 3</a>
-      <a href="#" class="btn btn-primary btn-lg">Link 4</a>
-    </div>
-  </div> -->
+</html>
