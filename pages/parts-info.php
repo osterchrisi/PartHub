76b33320f1953dd0d492676bfc318add3e56f68e
chrisi <chrisi@mobstationT.local>
2023-03-21 11:52:44 +0100

    Styling stock modals

--- pages/parts-info.php
+++ pages/parts-info.php
@@ -64,17 +64,16 @@ $total_stock = getTotalStock($result);
         <h1 class="modal-title fs-5" id="exampleModalLabel">Add Stock</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
       </div>
-      <div class="modal-body">
-        Add Stock for <?php echo $part_name;?>
-      </div>
+      <div class="modal-body mx-1">
+        Add Stock for <?php echo $part_name;?><br><br>
       <form>
       <input class="form-control stockModalNumber" placeholder="Quantity" id="addStockQuantity"><br>
       <input class="form-control" placeholder="Description / PO" id="addStockDescription">
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-        
       </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
     </div>
   </div>
 </div>
