02372fb6bcba15c5535c48654e97cbcaab19c64d
chrisi <chrisi@mobstationT.local>
2023-03-27 18:00:31 +0200

    Bootstrapped stock history table. It's glorious now

--- pages/stock-history.php
+++ pages/stock-history.php
@@ -2,14 +2,15 @@
 include '../config/stock-history-columns.php';
 $basename = basename(__FILE__);
 $stock_history = getPartStockHistory($conn, $part_id);
-// echo "<pre>";
-// print_r($stock_history);
-// echo "</pre>";
 ?>
 
 <div class="container-fluid">
     <!-- Stock History Table -->
 
-    <?php
-    buildTable($db_columns, $nice_columns, $stock_history);
-    ?>
+    <?php buildPartStockHistoryTable($db_columns, $nice_columns, $stock_history); ?>
+
+</div>
+
+<script>
+    bootstrapHistTable();
+</script>
