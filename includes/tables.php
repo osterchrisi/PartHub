3f17de44196fbf0d42610a5cdbf552e05bfcd8db
chrisi <chrisi@mobstationT.local>
2023-03-27 19:42:18 +0200

    Part-in-BOM table

--- includes/tables.php
+++ includes/tables.php
@@ -205,3 +205,46 @@ function buildPartStockHistoryTable($db_columns, $nice_columns, $stock_history, 
     echo "</table>";
     echo "</div>";
 }
+
+//! When I wrote this function I wasn't yet clear about what to use as the data-id. But only relevant, once this table should become editable
+function buildPartInBomsTable($db_columns, $nice_columns, $bom_list, $width = "100%")
+{
+    echo '<div class="table-responsive" style="overflow-x:auto; font-size:12px">';
+    echo '<table
+            class="table table-hover table-striped table-sm"
+            id="partStockHistoryTable"
+            data-resizable="true"
+            data-search="true"
+            data-search-align="left"
+            data-show-columns="true"
+            data-reorderable-columns="true"
+            data-cookie="true"
+            data-cookie-id-table="rememberTableState"
+            >';
+
+    // Table headers
+    echo "<thead class='table table-sm table-dark'>";
+    echo "<tr>";
+    foreach ($nice_columns as $column_header) {
+        echo "<th data-field='$column_header'>$column_header</th>";
+    }
+    echo "</tr>";
+    echo "</thead>";
+
+    echo "<tbody>";
+    // Table rows
+    foreach ($bom_list as $row) {
+        // echo "<tr>";
+        $bom_id = $row['bom_id'];
+        echo "<tr data-id=" . $row['bom_elements_id'] . ">";
+        foreach ($db_columns as $column_data) {
+            echo "<td data-editable='true' data-id=" . $bom_id . " data-column=" . $column_data . ">" . $row[$column_data] . "</td>";
+        }
+
+        echo "</tr>";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+    echo "</div>";
+}
