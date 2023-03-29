dbd7cc3c376bc8525893d4b956be402b0ad77fed
chrisi <chrisi@mobstationT.local>
2023-03-29 22:46:26 +0200

    User login

--- includes/verify-login.php
+++ includes/verify-login.php
@@ -6,9 +6,13 @@ $conn = connectToSQLDB($hostname, $username, $password, $database_name);
 $hash = getPasswordHash($conn, $_POST['email']);
 
 //! Not really happy with getting too much info at once? Maybe only password hash?
-if (password_verify($_POST['passwd'], $hash[0]['user_passwd'])){
-    $user = $hash[0]['user_name'];
-    echo "password correct, welcome $user";
+if (password_verify($_POST['passwd'], $hash[0]['user_passwd'])) {
+    session_destroy();
+    session_set_cookie_params(0, '/', '', true, true);
+    session_start();
+
+    $_SESSION['user_id'] = $hash[0]['user_id'];
+    header("Location: /PartHub/index.php");
 }
 else {
     echo "password incorrect";
