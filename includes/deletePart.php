8d4e31d992b1f6c73b58f2b602b2e7b19edba167
chrisi <chrisi@mobstationT.local>
2023-04-07 13:04:15 +0200

    Row deletion exception handling

--- includes/deletePart.php
+++ includes/deletePart.php
@@ -5,11 +5,12 @@ $part_ids = $_POST['part_ids'];
 
 // It's an array, even if it has just one entry, so I need to iterate here
 foreach ($part_ids as $part_id) {
+    echo "Deleting $part_id \n";
     try {
-        echo "Deleting $part_id ";
-        // $conn = connectToSQLDB($hostname, $username, $password, $database_name);
-        // deletePart($conn, $part_id);
+        $conn = connectToSQLDB($hostname, $username, $password, $database_name);
+        deletePart($conn, $part_id);
+        echo "Successfully deleted $part_id\n";
     } catch (Exception $e) {
-        echo "<br>Error: " . $e->getMessage();
+        echo "Error: " . $e->getMessage(), "\n";
     }
 }
