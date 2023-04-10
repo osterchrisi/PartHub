d04617d0192007f496c8e1a62e790d5f99e8a240
chrisi <chrisi@mobstationT.local>
2023-04-10 14:35:36 +0200

    Deleting parts

--- includes/deletePart.php
+++ includes/deletePart.php
@@ -2,15 +2,19 @@
 include '../config/credentials.php';
 include 'SQL.php';
 $part_ids = $_POST['part_ids'];
+$success = 0;
+$fail = 0;
 
 // It's an array, even if it has just one entry, so I need to iterate here
 foreach ($part_ids as $part_id) {
-    echo "Deleting $part_id \n";
     try {
         $conn = connectToSQLDB($hostname, $username, $password, $database_name);
         deletePart($conn, $part_id);
-        echo "Successfully deleted $part_id\n";
+        $success += 1;
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage(), "\n";
+        $fail += 1;
     }
 }
+
+// Properly return successfuly and failed parts
