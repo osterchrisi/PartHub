fc49fc87778d17aa5942ceb212b0306a0d0c2273
chrisi <chrisi@mobstationT.local>
2023-04-25 18:42:03 +0200

    validateForm callback

--- assets/js/partEntry.js
+++ assets/js/partEntry.js
@@ -2,42 +2,26 @@
 function callPartEntryModal(locations) {
   addPartLocationDropdown(locations)
   $('#mPartEntry').modal('show'); // Show modal
-  validateForm('partEntryForm', 'addPart');
+  validateForm('partEntryForm', 'addPart', addPartCallback);
 }
 
-// Validate required fields in part adding modal
-function validateForm(formId, button) {
+/**
+ * Validates required fields in a form when the specified button is clicked.
+ *
+ * @param {string} formId - The ID of the form to validate.
+ * @param {string} button - The ID of the button element to attach the click listener to.
+ * @param {function} callback - The function to execute when the form is submitted and valid.
+ */
+function validateForm(formId, button, callback) {
   const form = document.getElementById(formId);
   const submitBtn = document.getElementById(button);
 
   // Form validation
-  $('#addPart').click(function (event) {
-    // submitBtn.addEventListener('click', function (event) {
+  $(submitBtn).click(function (event) {
     event.preventDefault();
     if (form.checkValidity()) {
       // Form is valid
-      pn = $("#addPartName").val(); // Part Name
-      q = $("#addPartQuantity").val(); // Quantity
-      l = $("#addPartLocSelect").val(); // Location
-      console.log(pn, q, l);
-
-      // Inset new part into table
-      $.post('/PartHub/includes/create-part.php',
-        { part_name: pn, quantity: q, to_location: l },
-        function (response) {
-          // Response contains 'Part ID', 'Stock Entry ID' and 'Stock Level History ID'
-          var partId = JSON.parse(response)["Part ID"];
-          updatePartsInfo(partId);
-          $('#mPartEntry').modal('hide'); // Hide modal
-          removeClickListeners('#addPart'); // Remove click listener from Add Part button
-
-          // Rebuild parts table and select new row
-          var queryString = window.location.search;
-          $.when(rebuildPartsTable(queryString)).done(function () {
-            $('tr[data-id="' + partId + '"]').addClass('selected selected-last');
-          });
-        });
-
+      callback();
     } else {
       // Form is invalid (required fields not filled)
       form.querySelectorAll('[required]').forEach(function (field) {
@@ -51,6 +35,30 @@ function validateForm(formId, button) {
       });
     }
   });
+}
+
+function addPartCallback() {
+  pn = $("#addPartName").val(); // Part Name
+  q = $("#addPartQuantity").val(); // Quantity
+  l = $("#addPartLocSelect").val(); // Location
+  console.log(pn, q, l);
+
+  // Inset new part into table
+  $.post('/PartHub/includes/create-part.php',
+    { part_name: pn, quantity: q, to_location: l },
+    function (response) {
+      // Response contains 'Part ID', 'Stock Entry ID' and 'Stock Level History ID'
+      var partId = JSON.parse(response)["Part ID"];
+      updatePartsInfo(partId);
+      $('#mPartEntry').modal('hide'); // Hide modal
+      removeClickListeners('#addPart'); // Remove click listener from Add Part button
+
+      // Rebuild parts table and select new row
+      var queryString = window.location.search;
+      $.when(rebuildPartsTable(queryString)).done(function () {
+        $('tr[data-id="' + partId + '"]').addClass('selected selected-last');
+      });
+    });
 }
 
 // Create the part entry location dropdown
