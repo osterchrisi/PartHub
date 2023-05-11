59ef2dc175ba1fb60cb5d65c245c575d02f906ae
chrisi <chrisi@mobstationT.local>
2023-05-11 17:33:04 +0200

    Fixed welcome view again after breeze broke it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,3 @@
-<?php 
-// include 'session.php'; 
-?>
-
 <!doctype html>
 <html lang="en">
 
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['title' => 'Welcome']);
 });
 
 Route::get('/dashboard', function () {
