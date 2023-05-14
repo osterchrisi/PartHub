7eb83315beb074e789db295b8b4b8f4b8dad0483
chrisi <chrisi@mobstationT.local>
2023-05-15 00:00:43 +0200

    StockLevel model

--- app/Models/StockLevel.php
+++ app/Models/StockLevel.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class StockLevelChangeHistory extends Model
+class StockLevel extends Model
 {
     use HasFactory;
+    protected $table = 'stock_levels';
+    protected $primaryKey = 'stock_level_id';
+
 }
--- app/Models/StockLevelHistory.php
+++ app/Models/StockLevelHistory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class StockLevels extends Model
+class StockLevelHistory extends Model
 {
     use HasFactory;
 }
