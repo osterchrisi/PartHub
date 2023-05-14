8a7dc01af7da0746ca6a05f3400c6d32db1366a4
chrisi <chrisi@mobstationT.local>
2023-05-14 12:41:42 +0200

    Code cleanup

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
