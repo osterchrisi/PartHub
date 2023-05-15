3fd56b7d36ebdc91bdfe6c190c0f71427df32b0c
chrisi <chrisi@mobstationT.local>
2023-05-16 00:51:43 +0200

    'Naked' part info

--- resources/views/parts/showPart.blade.php
+++ resources/views/parts/showPart.blade.php
@@ -1,1 +1,105 @@
-<h4>Details for {{ $part_id }}</h4>
+<?php
+// Get part ID
+// $part_id = {{ $part_id }}
+
+// Get part name
+// $result = getPartName($conn, $part_id);
+// $part_name = $result[0]['part_name'];
+
+// Get stock levels
+// $stock_levels = getStockLevels($conn, $part_id);
+// $total_stock = getTotalStock($stock_levels);
+
+// Putting it into the session array for stockChanges.php to use
+// $_SESSION['stock_levels'] = $stock_levels;
+
+// Get available locations
+// $locations = getLocations($conn, $user_id);
+?>
+
+<div class="container-fluid">
+  <br>
+  <h4>
+    <?php 
+        // echo $part_name; 
+        ?>
+  </h4>
+  <h5>Total stock:
+    <?php
+        // echo $total_stock; 
+        ?>
+  </h5>
+
+  <!-- Parts Tabs -->
+  <ul class="nav nav-tabs" id="partsTabs" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="partStockInfoTabToggler" data-bs-toggle="tab" data-bs-target="#partStockInfoTab"
+        type="button" role="tab">Info</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="partStockHistoryTabToggler" data-bs-toggle="tab"
+        data-bs-target="#partStockHistoryTab" type="button" role="tab">Stock History</button>
+    </li>
+  </ul>
+
+  <!-- Tabs Content -->
+  <div class="tab-content" id="partsTabsContent">
+    <div class="tab-pane fade" id="partStockInfoTab" role="tabpanel" tabindex="0">
+      <br>
+      <!-- Location / Quantity Table -->
+      <?php 
+    //   buildTable($db_columns, $nice_columns, $stock_levels); 
+      ?>
+
+      <!-- Stock movement buttons -->
+      <div class="input-group">
+        <input type="text" class="form-control form-control-sm" placeholder="Stock:" disabled readonly>
+        <button type="button" class="btn btn-sm btn-outline-primary"
+          onclick='callStockModal("1", <?php 
+        //   echo json_encode($locations) . ", $part_id"; 
+          ?>);'>Add</button>
+        <button type="button" class="btn btn-sm btn-outline-primary"
+          onclick='callStockModal("0", <?php 
+        //   echo json_encode($locations) . ", $part_id"; 
+          ?>);'>Move</button>
+        <button type="button" class="btn btn-sm btn-outline-primary"
+          onclick='callStockModal("-1", <?php 
+        //   echo json_encode($locations) . ", $part_id"; 
+          ?>);'>Reduce</button>
+      </div>
+      <br><br>
+
+      <h5>Part of:</h5>
+      <?php
+    //   include '../config/part-in-boms-columns.php';
+    //   $bom_list = getPartInBoms($conn, $part_id);
+    //   buildPartInBomsTable($db_columns, $nice_columns, $bom_list); ?>
+      <br>
+      <h5>Datasheet:</h5>
+      <br>
+      <h5>Image:</h5>
+
+    </div>
+    <div class="tab-pane fade" id="partStockHistoryTab" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+      <?php 
+        // include 'stock-history.php'; 
+        ?>
+    </div>
+  </div>
+</div>
+
+<!-- Include custom JS -->
+<script>
+  <?php 
+//   include '../assets/js/stockChanges.js'; 
+  ?>
+  <?php 
+//   include '../assets/js/tables.js'; 
+  ?>
+  $(document).ready(function () {
+    loadActiveTab();
+    addActiveTabEventListeners();
+    bootstrapPartInBomsTable();
+    bootstrapTableSmallify();
+  });
+</script>
