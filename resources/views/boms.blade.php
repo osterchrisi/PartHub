d970e1089653b63ebe836b3c17062898bcbd2810
chrisi <chrisi@mobstationT.local>
2023-05-18 19:56:48 +0200

    Adding app template

--- resources/views/boms.blade.php
+++ resources/views/boms.blade.php
@@ -6,4 +6,12 @@
 {{-- Visible Page Contents --}}
 @include('header')
 @include('navbar')
-@include('components.toolbarTop')
+@extends('components.toolbarTop')
+
+@section('page-specific-buttons')
+    <li class="nav-item p-1">
+        <button type="button" class="btn btn-sm btn-primary btn-labeled" data-bs-toggle="collapse"
+            data-bs-target="#parts-filter-form"><span class="btn-label"><i
+                    class="fas fa-lg fa-wrench"></i></span>Assemble</button>
+    </li>
+@endsection
