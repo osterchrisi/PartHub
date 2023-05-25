c3b32d31faf7326a5f39880020ab8b8c21593b15
chrisi <chrisi@mobstationT.local>
2023-05-25 19:02:19 +0200

    Making components for info tabs

--- resources/views/boms/showBom.blade.php
+++ resources/views/boms/showBom.blade.php
@@ -3,58 +3,34 @@
     <h4>
         {{ $bom_name }}
     </h4>
-    <h5>Total stock:
+    <h5>
         {{ $bom_description }}
     </h5>
 
     <!-- Parts Tabs -->
-    {{-- <ul class="nav nav-tabs" id="partsTabs" role="tablist">
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" id="partStockInfoTabToggler" data-bs-toggle="tab" data-bs-target="#partStockInfoTab"
-                type="button" role="tab">Info</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" id="partStockHistoryTabToggler" data-bs-toggle="tab"
-                data-bs-target="#partStockHistoryTab" type="button" role="tab">Stock History</button>
-        </li>
-    </ul> --}}
+    <ul class="nav nav-tabs" id="bomsTabs" role="tablist">
+        <x-tab-button id="bomsTab1" toggleTarget="bomInfo" tabText="Info" />
+        <x-tab-button id="bomsTab2" toggleTarget="bomBuildHistory" tabText="Build History" />
+    </ul>
 
     <!-- Tabs Content -->
-    {{-- <div class="tab-content" id="partsTabsContent">
-        <div class="tab-pane fade" id="partStockInfoTab" role="tabpanel" tabindex="0">
+    <div class="tab-content" id="bomsTabsContent">
+        <div class="tab-pane fade" id="bomInfo" role="tabpanel" tabindex="1">
             <br>
-            <!-- Location / Quantity Table -->
-            @include('parts.stockTable')
-
-
-            <!-- Stock movement buttons -->
-            <div class="input-group">
-                <input type="text" class="form-control form-control-sm" placeholder="Stock:" disabled readonly>
-                <button type="button" class="btn btn-sm btn-outline-primary" id="addStockButton">Add</button>
-                <button type="button" class="btn btn-sm btn-outline-primary" id="moveStockButton">Move</button>
-                <button type="button" class="btn btn-sm btn-outline-primary" id="reduceStockButton">Reduce</button>
-            </div>
-            <br><br>
-
-            <h5>Part of:</h5>
-
-            @include('parts.bomTable')
-            
-            <br>
-            <h5>Datasheet:</h5>
-            <br>
-            <h5>Image:</h5>
-
+            dis info
         </div>
-        <div class="tab-pane fade" id="partStockHistoryTab" role="tabpanel" aria-labelledby="profile-tab"
-            tabindex="0">
 
-            @include('parts.stockHistoryTable')
-
+        <div class="tab-pane fade" id="bomBuildHistory" role="tabpanel" tabindex="0">
+            <br>
+            dis history
         </div>
-    </div> --}}
+    </div>
 </div>
 
 <!-- Include custom JS -->
 <script>
+    // $(document).ready(function() {
+    //     loadActiveTab();
+    //     addActiveTabEventListeners();
+    // });
 </script>
---
+++ resources/views/components/tab-button.blade.php
@@ -0,0 +1,4 @@
+<li class="nav-item" role="presentation">
+    <button class="nav-link" id="{{ $id }}" data-bs-toggle="tab" data-bs-target="#{{ $toggleTarget }}" type="button"
+        role="tab">{{ $tabText }} </button>
+</li>
