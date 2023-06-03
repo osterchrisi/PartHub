8f31d9a807c9e4d8291c1e2d3e8ecae611fe7d80
chrisi <chrisi@mobstationT.local>
2023-06-03 11:32:58 +0200

    And yet more color customization

--- resources/views/components/tab-button.blade.php
+++ resources/views/components/tab-button.blade.php
@@ -1,4 +1,4 @@
 <li class="nav-item" role="presentation">
-    <button class="nav-link" id="{{ $id }}" data-bs-toggle="tab" data-bs-target="#{{ $toggleTarget }}" type="button"
+    <button class="nav-link tab-link" id="{{ $id }}" data-bs-toggle="tab" data-bs-target="#{{ $toggleTarget }}" type="button"
         role="tab">{{ $tabText }} </button>
 </li>
