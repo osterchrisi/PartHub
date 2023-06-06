2ceb83abd7954b3333ff076b27ed2b0deaf6e56a
chrisi <chrisi@mobstationT.local>
2023-06-06 11:48:55 +0200

    Code cleanup

--- app/Imports/BomImport.php
+++ app/Imports/BomImport.php
@@ -21,30 +21,32 @@ class BomImport implements ToModel, WithHeadingRow
     }
     public function model(array $row)
     {
-        $part_name = $row['Part Number'];
+        $part_name = $row['Part Name'];
         $part_id = $row['Part ID'];
-        $quantity = $row['quantity'];
+        $quantity = $row['Quantity'];
+        $user_id = auth()->user()->id;
 
         // Check if both part number and part ID are provided
         if (!empty($part_name) && !empty($part_id)) {
-            // Perform a database query to verify if the part number and part ID match
-            $part = Part::where('part_number', $part_name)
+            // Perform query to verify if the part number and part ID match
+            $part = Part::where('part_name', $part_name)
                         ->where('part_id', $part_id)
+                        ->where('part_owner_u_fk', $user_id)
                         ->first();
 
             if (!$part) {
-                // Part number and part ID do not match, handle the error
+                // Part number and part ID do not match,
                 throw new \Exception('Part number and part ID do not match for row: ' . print_r($row, true));
             }
         } else if (empty($part_name) && empty($part_id)) {
-            // Both part number and part ID are empty, handle the error
+            // Both part number and part ID are empty
             throw new \Exception('Both part number and part ID are missing for row: ' . print_r($row, true));
         } else {
             // Either part number or part ID is provided, handle accordingly
-            $part = $part_name ? Part::where('part_number', $part_name)->first() : Part::find($part_id);
+            $part = $part_name ? Part::where('part_number', $part_name)->where('part_owner_u_fk', $user_id)->first() : Part::find($part_id)->where('part_owner_u_fk', $user_id);
 
             if (!$part) {
-                // Part number or part ID does not exist, handle the error
+                // Part number or part ID does not exist
                 throw new \Exception('Invalid part number or part ID for row: ' . print_r($row, true));
             }
         }
