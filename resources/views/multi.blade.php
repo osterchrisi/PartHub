b0137db2e9de9f5fe40b42cc4b58016759382a11
chrisi <chrisi@mobstationT.local>
2023-06-17 11:32:20 +0200

    Multi view

--- resources/views/multi.blade.php
+++ resources/views/multi.blade.php
@@ -24,25 +24,25 @@
         </div>
 
         <div class='row'>
-            <div class="collapse collapse-horizontal bg-primary" id="collapseWidthExample" style='width: 200px;'>
+            {{-- <div class="collapse collapse-horizontal bg-primary w-100" id="collapseWidthExample"> --}}
                 {{-- Table Window --}}
-                <div class='col' id='table-window'>
+                <div class='col' id='table-window' style='max-width: 90%;'>
                 </div>
 
                 {{-- Info Window --}}
-                {{-- <div class='col d-flex resizable sticky justify-content-center info-window pb-3' id='info-window'
-                    style="position: sticky; top: 50px; height: 89vh;">
-                </div> --}}
-            </div>
+                <div class='col d-flex resizable sticky justify-content-center info-window pb-3' id='info-window'
+                    style="position: sticky; top: 50px;">
+                </div>
+            {{-- </div> --}}
 
             {{-- Table Window 2 --}}
-            <div class='col' id='table-window2' style='max-width: 90%;'>
-            </div>
+            {{-- <div class='col' id='table-window2'>
+            </div> --}}
 
             {{-- Info Window 2 --}}
-            <div class='col d-flex resizable sticky justify-content-center info-window pb-3' id='info-window2'
-                style="position: sticky; top: 50px; height: 89vh;">
-            </div>
+            {{-- <div class='col d-flex resizable sticky justify-content-center info-window pb-3' id='info-window2'
+                style="position: sticky; top: 50px;">
+            </div> --}}
         </div>
 
     </div>
