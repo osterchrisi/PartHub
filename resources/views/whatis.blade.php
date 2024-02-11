b1fb88f4718cbf3ebd6f3064cc755f0f15e73c08
chrisi <chrisi@mobstationT.local>
2024-02-11 23:34:40 +0100

    What Is view

--- resources/views/whatis.blade.php
+++ resources/views/whatis.blade.php
@@ -6,9 +6,26 @@
         <tr>
             <td colspan="3">
                 <h1 class="display-1" id="whatis-headline">What is PartHub?</h1><br>
-                <h4>I am an</h2><br>
-                <h1>Inventory and BOM management</h1><br>
-                I am a tool to help you keep your workplace tidy!
+                <h4>TLDR; I am an</h2><h1>Inventory and BOM management software</h1><br>
+                <p>...I am here to help you keep your workplace tidy</h5><br>
+                    I take care of the boring stock keeping and you can focus on the creative part of your work.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <h1 class="display-1" id="whatis-headline">How can PartHub help me?</h1><br>
+                {{-- <h4>TLDR; I am an</h2><h1>Inventory and BOM management software</h1><br> --}}
+                <p>I make tracking stock fun. It's like a zombie shooter video game.<br>
+                    Only that you use a barcode scanner as a gun and shoot labels instead of zombies.<br>
+                (Also works without a barcode scanner)</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <h1 class="display-1" id="whatis-headline">Is PartHub for me?</h1><br>
+                {{-- <h4>TLDR; I am an</h2><h1>Inventory and BOM management software</h1><br> --}}
+                <p>Yes! If you buy electronic parts for your projects and they come in small baggies with labels,<br>
+                    I am here to make your life significantly easier!</p>
             </td>
         </tr>
         {{-- <tr>
