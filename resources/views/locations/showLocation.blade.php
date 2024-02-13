0b265a67e0e6ea542fff7f403460f8129cb11dfc
chrisi <chrisi@mobstationT.local>
2024-02-13 21:50:37 +0100

    Code cleanup

--- resources/views/locations/showLocation.blade.php
+++ resources/views/locations/showLocation.blade.php
@@ -7,7 +7,7 @@
         {{ $location_description }}
     </h5>
 
-    <!-- Parts Tabs -->
+    <!-- Location Tabs -->
     <ul class="nav nav-tabs" id="bomsTabs" role="tablist">
         <x-tab-button id="{{ $tabId1 }}" toggleTarget="{{ $tabToggleId1 }}" tabText="{{ $tabText1 }}" />
         <x-tab-button id="{{ $tabId2 }}" toggleTarget="{{ $tabToggleId2 }}" tabText="{{ $tabText2 }}" />
@@ -18,7 +18,6 @@
         <div class="tab-pane fade" id="{{ $tabToggleId1 }}" role="tabpanel" tabindex="0">
             <br>
             <h5>Stock in this Location</h5>
-            {{-- @include('locations.locationDetailsTable') --}}
             @php
             $table = \buildHTMLTable($db_columns, $nice_columns, $stock_in_location);
             echo $table;
