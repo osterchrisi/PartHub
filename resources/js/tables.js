ee811a78cf5153619f995484e98ab10a812db817
chrisi <chrisi@mobstationT.local>
2024-02-19 17:52:48 +0100

    Function extraction, commenting and cleanup

--- resources/js/tables.js
+++ resources/js/tables.js
@@ -393,9 +393,127 @@ function hideContextMenu($menu) {
   $menu.hide();
 }
 
-// Inline table cell manipulation of bootstrapped tables
-//TODO: Extract functions
-//TODO: Remove dropdown upon selecting same option again
+/**
+ * Inline table cell editing of a text cell in the parts table
+ * @param {jQuery} cell The cell being edited
+ * @param {string} originalValue The original value of the cell before editing
+ */
+function editTextCell(cell, originalValue) {
+  // Create input field
+  var input = $('<textarea class="form-control">').val(originalValue);
+  cell.empty().append(input);
+  input.focus();
+
+  // Create label for input field
+  var label = $('<small class="text-muted">Enter: Confirm</small>');
+  cell.append(label);
+
+  // Confirm upon pressing Enter key
+  input.keypress(function (event) {
+    if (event.keyCode === 13) {
+      input.blur();
+    }
+  });
+
+  // Close input on "Escape" key press
+  input.on('keydown', function (event) {
+    if (event.key === "Escape") {
+      input.remove();
+      cell.text(originalValue);
+      cell.removeClass('editing');
+      return;
+    }
+  });
+
+  // Enter new value
+  input.blur(function () {
+    // Get newly entered value
+    var new_value = input.val();
+
+    // Update cell with new value
+    cell.text(new_value);
+
+    // Get cell id, column name and database table
+    // These are encoded in the table data cells
+    var id = cell.closest('td').data('id');
+    var column = cell.closest('td').data('column');
+    var table_name = cell.closest('td').data('table_name');
+    var id_field = cell.closest('td').data('id_field');
+    console.log(id, id_field, column, table_name, new_value);
+
+    // Call the updating function
+    updateCell(id, column, table_name, new_value, id_field);
+    cell.removeClass('editing');
+  });
+}
+
+/**
+ * Inline table cell editing of a category cell in the parts table
+ * @param {jQuery} cell The cell being edited
+ * @param {string} originalValue The original value of the cell before editing
+ */
+function editCategoryCell(cell, originalValue) {
+  console.log(typeof (cell))
+  console.log(typeof (originalValue))
+  // Changed flag
+  var valueChanged = false;
+  // Get list of available categories and populate dropdown
+  var categories = $.ajax({
+    type: 'GET',
+    url: '/categories.list',
+    dataType: 'JSON',
+    success: function (response) {
+      categories = response;
+
+      // Create select element
+      var select = createInlineCategorySelect(categories, originalValue);
+
+      // Append, selectize category dropdown
+      appendInlineCategorySelect(cell, select);
+
+      // Need to focus the selectize control
+      var selectizeControl = select[0].selectize;
+      selectizeControl.focus();
+
+      // Select element change event handler and callback function to set flag
+      inlineCategorySelectEventHandler(select, cell, categories, function changeFlagCallback() {
+        valueChanged = true;
+      });
+
+      // Listen for the blur event on the selectize control
+      selectizeControl.on('blur', function () {
+        // Remove the select element when the selectize dropdown loses focus
+        select.remove();
+        // Change cell text back if value was not changed
+        if (!valueChanged) {
+          cell.text(originalValue);
+        }
+        cell.removeClass('editing');
+      });
+
+      // Listen for the Escape keydown event on the document level because selectized element is eating my events
+      $(document).on('keydown', function (event) {
+        if (event.key === "Escape" && cell.hasClass('editable') && cell.hasClass('category') && cell.hasClass('editing')) {
+          select.remove();
+          // Change cell text back if value was not changed
+          if (!valueChanged) {
+            cell.text(originalValue);
+          }
+          cell.removeClass('editing');
+          // Remove the event handler once it has done its job
+          $(document).off('keydown');
+        }
+      });
+
+
+
+    }
+  });
+}
+
+/**
+ * Inline table cell manipulation of bootstrapped tables
+ */
 export function inlineProcessing() {
   $('.bootstrap-table').on('dblclick', '.editable', function (e) {
     var cell = $(this);
@@ -414,108 +532,10 @@ export function inlineProcessing() {
 
     // * It's a category cell
     if (cell.hasClass('category')) {
-      // Changed flag
-      var valueChanged = false;
-      // Get list of available categories and populate dropdown
-      var categories = $.ajax({
-        type: 'GET',
-        url: '/categories.list',
-        dataType: 'JSON',
-        success: function (response) {
-          categories = response;
-
-          // Create select element
-          var select = createInlineCategorySelect(categories, originalValue);
-
-          // Append, selectize category dropdown
-          appendInlineCategorySelect(cell, select);
-
-          // Need to focus the selectize control
-          var selectizeControl = select[0].selectize;
-          selectizeControl.focus();
-
-          // Select element change event handler and callback function to set flag
-          inlineCategorySelectEventHandler(select, cell, categories, function changeFlagCallback() {
-            valueChanged = true;
-          });
-
-          // Listen for the blur event on the selectize control
-          selectizeControl.on('blur', function () {
-            // Remove the select element when the selectize dropdown loses focus
-            select.remove();
-            // Change cell text back if value was not changed
-            if (!valueChanged) {
-              cell.text(originalValue);
-            }
-            cell.removeClass('editing');
-          });
-
-          // Listen for the Escape keydown event on the document level because selectized element is eating my events
-          $(document).on('keydown', function (event) {
-            if (event.key === "Escape" && cell.hasClass('editable') && cell.hasClass('category') && cell.hasClass('editing')) {
-              select.remove();
-              // Change cell text back if value was not changed
-              if (!valueChanged) {
-                cell.text(originalValue);
-              }
-              cell.removeClass('editing');
-              // Remove the event handler once it has done its job
-              $(document).off('keydown');
-            }
-          });
-
-
-
-        }
-      });
+      editCategoryCell(cell, originalValue);
     }
     else { // * It's a text cell
-      // Create input field
-      var input = $('<textarea class="form-control">').val(originalValue);
-      cell.empty().append(input);
-      input.focus();
-
-      // Create label for input field
-      var label = $('<small class="text-muted">Enter: Confirm</small>');
-      cell.append(label);
-
-      // Confirm upon pressing Enter key
-      input.keypress(function (event) {
-        if (event.keyCode === 13) {
-          input.blur();
-        }
-      });
-
-      // Close input on "Escape" key press
-      input.on('keydown', function (event) {
-        if (event.key === "Escape") {
-          input.remove();
-          cell.text(originalValue);
-          cell.removeClass('editing');
-          return;
-        }
-      });
-
-      // Enter new value
-      input.blur(function () {
-        // Get newly entered value
-        var new_value = input.val();
-
-        // Update cell with new value
-        cell.text(new_value);
-
-        // Get cell id, column name and database table
-        // These are encoded in the table data cells
-        var id = cell.closest('td').data('id');
-        var column = cell.closest('td').data('column');
-        var table_name = cell.closest('td').data('table_name');
-        var id_field = cell.closest('td').data('id_field');
-        console.log(id, id_field, column, table_name, new_value);
-
-        // Call the updating function
-        updateCell(id, column, table_name, new_value, id_field);
-        cell.removeClass('editing');
-      });
+      editTextCell(cell, originalValue);
     }
   });
 };
