d72d397e3286d41c36f08262f4270d201f7afc0e
chrisi <chrisi@mobstationT.local>
2024-02-20 00:11:51 +0100

    Removed register route from guest middleware

--- routes/auth.php
+++ routes/auth.php
@@ -11,10 +11,10 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
+
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create']);
-
-    Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
