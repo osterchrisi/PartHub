fac2bbeee8ba9834fd3ff73efa25df4e7de3e108
chrisi <chrisi@mobstationT.local>
2024-02-21 00:28:06 +0100

    Added APP_FAVICON variable and tuned welcome mail

--- resources/views/mail/WelcomeEmail.blade.php
+++ resources/views/mail/WelcomeEmail.blade.php
@@ -1,27 +1,30 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Welcome Template</title>
     <style>
         body {
-            font-family: system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue","Noto Sans","Liberation Sans",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+            font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
             font-size: 1rem;
         }
     </style>
 </head>
+
 <body>
-    <div style="text-align: center;">
-<img src="https://parthub.online/favicon.ico" alt="PartHub Logo" style="width: 5%"></img>
-<br>
-<h1>Thank you for chosing PartHub, {{$name}}!</h1><br><br>
-<p>Your user account has been created and you can start adding parts and BOMs right away.<br>
-    <!-- Make sure to check out our import function to speed up this process!<br> -->
-    <br>
-    All the best,<br>
-    the PartHub team from Berlin
-</p>
-</div>
+    <div>
+        <h1>Thank you for chosing PartHub, {{ $name }}!</h1>
+        <h4>We're glad to have you onboard 🚀 </h4>
+        <p>
+            Your user account has been created and you can start adding parts right away.<br>
+            If you have any questions or feedback don't hesitate to contact us at: <a href="mailto:hello@parthub.online">hello@parthub.online</a><br>
+            <br>
+            All the best,<br>
+            the PartHub team from Berlin<br><br>
+            <img src="{{ env('APP_FAVICON') }}" alt="PartHub Logo"></img>
+        </p>
+    </div>
 </body>
+
 </html>
