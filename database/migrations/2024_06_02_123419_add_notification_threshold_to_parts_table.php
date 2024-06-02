242e164e8ad119f9fc4f05d6b1097829164a4f6b
chrisi <chrisi@mobstationT.local>
2024-06-02 14:36:04 +0200

    Modify notification threshold to parts table

--- database/migrations/2024_06_02_123419_add_notification_threshold_to_parts_table.php
+++ database/migrations/2024_06_02_123419_add_notification_threshold_to_parts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('parts', function (Blueprint $table) {
-            $table->integer('notification_threshold')->default(0);
+            $table->integer('stocklevel_notification_threshold')->default(0);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('parts', function (Blueprint $table) {
-            $table->dropColumn('notification_threshold');
+            $table->dropColumn('stocklevel_notification_threshold');
         });
     }
 };
