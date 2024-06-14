7a4c238c6784cbdb8d5319641990dbf21c610df0
chrisi <chrisi@mobstationT.local>
2024-06-14 18:32:14 +0200

    X-Error styling

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <div class="mx-0 px-0 my-0 px-0 alert alert-danger">
+    <div class="mx-0 my-0 alert alert-danger pt-3 pb-0 ps-0">
         <ul {{ $attributes->merge(['class' => 'text-sm']) }}>
             @foreach ((array) $messages as $message)
                 <li>{{ $message }}</li>
