8c6cc353c51cc3bccb161edc9e47974d971fb6e5
chrisi <chrisi@mobstationT.local>
2024-07-09 00:29:21 +0200

    TOS

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -5,14 +5,11 @@
     <table class="table table-borderless text-center mx-auto w-auto" style="borders: false">
         <tr>
             <td>
-                PartHub is a subsidiary of:<br>
-                KOMA Elektronik GmbH<br>
+                PartHub<br>
                 Koloniestr. 29<br>
                 13359 Berlin<br>
                 Germany<br>
-                VAT ID Number: DE285522050<br>
-                Registration Number: HRB 145453<br>
-                Court of Registration: Amtsgericht Berlin-Charlottenburg
+                Court of Registration: Amtsgericht Mitte
             </td>
         </tr>
     </table>
