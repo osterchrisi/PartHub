28bcc730511df97fa05a0349a3ed13aeea2814a3
chrisi <chrisi@mobstationT.local>
2024-07-08 23:55:59 +0200

    More UI

--- resources/views/centered-layout.blade.php
+++ resources/views/centered-layout.blade.php
@@ -1,7 +1,7 @@
 @include('header')
 @include('navbar')
 
-<div class="d-flex flex-grow-1 justify-content-center align-items-center {{ $view == 'whatis' ? 'landing-page' : '' }}">
+<div class="d-flex flex-grow-1 justify-content-center align-items-center {{ $view == 'whatis' || 'signup' ? 'landing-page' : '' }}">
     <div class="greeting d-flex align-items-center">
         @yield('content')
     </div>
