5385dd341c0fd5a26e4e1107bb01a340ab49d194
chrisi <chrisi@mobstationT.local>
2024-07-27 00:54:53 +0200

    Versioning UI

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,21 +4,21 @@ $version = file_get_contents('VERSION');
 
 
 <div class="container-fluid px-0 bg-body-tertiary text-dark" style="z-index: 2;">
-    <table class="table table-bordered text-center px-0 mx-auto w-auto" style="borders: false">
+    <table class="table table-borderless text-center mx-auto w-auto footer-table">
         <tr>
             <td>
                 <div>
-                    <p class="fs-6 lead" data-bs-toggle="tooltip" data-bs-title="{{ $version }}">&copy; 2024 PartHub. All rights reserved.</p>
+                    <span class="fs-6 lead">&copy; 2024 </span><span class="fs-6 lead" data-bs-toggle="tooltip" data-bs-title="{{ $version }}">PartHub</span><span class="fs-6 lead">. All rights reserved.</p>
                 </div>
             </td>
             <td>
                 <div>
-                    <p class="fs-6 lead"><a href="{{ route('TOS') }}">Terms of Service</a></p>
+                    <span class="fs-6 lead"><a href="{{ route('TOS') }}">Terms of Service</a></p>
                 </div>
             </td>
             <td>
                 <div>
-                    <p class="fs-6 lead"><a href="{{ route('imprint') }}">Imprint</a></p>
+                    <span class="fs-6 lead"><a href="{{ route('imprint') }}">Imprint</a></p>
                 </div>
             </td>
         </tr>
