9ea8d523a7fbb662a4ab887abf2d0232b1956ff2
chrisi <chrisi@mobstationT.local>
2024-07-27 00:43:15 +0200

    Versioning in footer

--- resources/js/app.js
+++ resources/js/app.js
@@ -66,4 +66,7 @@ $(document).ready(function () {
         }
     });
 
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 });
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,14 @@
+<?php
+$version = file_get_contents('VERSION');
+?>
+
+
 <div class="container-fluid px-0 bg-body-tertiary text-dark" style="z-index: 2;">
-    <table class="table table-borderless text-center mx-auto w-auto" style="borders: false">
+    <table class="table table-bordered text-center px-0 mx-auto w-auto" style="borders: false">
         <tr>
             <td>
                 <div>
-                    <p class="fs-6 lead">&copy; 2024 PartHub. All rights reserved.</p>
+                    <p class="fs-6 lead" data-bs-toggle="tooltip" data-bs-title="{{ $version }}">&copy; 2024 PartHub. All rights reserved.</p>
                 </div>
             </td>
             <td>
