4740ef73f6c146f008d077c610e8a307eaf2ea7c
chrisi <chrisi@mobstationT.local>
2024-07-27 22:31:16 +0200

    503 UX

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,8 @@
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
-@section('message', __('Service Unavailable'))
+{{-- @section('message', __('Service Unavailable - Maintenance'))
+ --}}
+ @section('message')
+    {!! __('Service Unavailable<br><br>We are currently undergoing scheduled maintenance. Please try again later.') !!}
+@endsection
