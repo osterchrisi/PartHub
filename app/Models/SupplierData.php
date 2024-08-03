48787cef23d53af05b7e88af5224abfd436aa784
chrisi <chrisi@mobstationT.local>
2024-08-03 21:20:49 +0200

    Supplier Data model and migration improvements

--- app/Models/SupplierData.php
+++ app/Models/SupplierData.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class SupplierData extends Model
 {
     use HasFactory;
+
+    private static $table_name = 'supplier_data';
+
+    public function part()
+    {
+        return $this->belongsTo(Part::class, 'part_id_fk', 'part_id');
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id_fk', 'supplier_id');
+    }
 }
--- database/migrations/2024_08_03_140546_create_supplier_data_table.php
+++ database/migrations/2024_08_03_140546_create_supplier_data_table.php
@@ -12,17 +12,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('supplier_data', function (Blueprint $table) {
-            $table->id();                       // Primary key
-            $table->unsignedBigInteger('part_id_fk'); // Foreign key for parts table
-            $table->unsignedBigInteger('supplier_id_fk'); // Foreign key for suppliers table
-            $table->string('URL')->nullable();  // Part URL
-            $table->string('SPN');              // Supplier Part Number
-            $table->decimal('price', 20, 6);    // Price with precision 20 and scale 6
-            $table->timestamps();               // Created_at and updated_at columns
+            $table->id();                                   // Primary key
+            $table->integer('part_id_fk');                  // Foreign key for parts table
+            $table->integer('supplier_id_fk');              // Foreign key for suppliers table
+            $table->string('URL')->nullable();              // Part URL
+            $table->string('SPN');                          // Supplier Part Number
+            $table->decimal('price', 20, 6);                // Price with precision 20 and scale 6
+            $table->timestamps();                           // Created_at and updated_at columns
 
             // Foreign key constraints
-            $table->foreign('part_id_fk')->references('id')->on('parts')->onDelete('cascade');
-            $table->foreign('supplier_id_fk')->references('id')->on('suppliers')->onDelete('cascade');
+            $table->foreign('part_id_fk')->references('part_id')->on('parts');
+            $table->foreign('supplier_id_fk')->references('supplier_id')->on('suppliers');
         });
     }
 
@@ -31,6 +31,12 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('supplier_data', function (Blueprint $table) {
+            // Drop foreign key constraints
+            $table->dropForeign(['part_id_fk']);
+            $table->dropForeign(['supplier_id_fk']);
+        });
+
         Schema::dropIfExists('supplier_data');
     }
 };
