bb8f688dbe15f007829e6269f4170106e7037f23
chrisi <chrisi@mobstationT.local>
2024-08-04 01:33:31 +0200

    whatis

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,92 +1,69 @@
 @extends('centered-layout')
 @section('content')
     <div class="row">
-        {{-- <div class="col-3"></div> --}}
         <div class="col text-center">
             <h1 class="display-1" id="welcome-headline">PartHub</h1><br>
             <h1>Pricing</h1><br>
 
             <p style="line-height: 1.5;">Focus on the fun part of making things<br>
-                Let PartHub tak care of the annoying tasks of stock keeping for you!<br><br>
-                Currently PartHub is in beta<br>
-                and it's a really good time to snatch afree
+                Let PartHub take care of the annoying tasks of stock keeping for you!<br><br>
+                Currently PartHub is in beta
+                and it's a really good time to snatch a free
                 early-bird account!</p>
             <p class="lead fw-bold"> There will always be a free tier for the enthusiasts!</p>
         </div>
-        <div class="col bg-light pt-2">
-            {{-- <table class="table table-borderless text-center mx-auto w-auto" style="borders: false">
-                <thead>
-                    <tr>
-                        <th> --}}
-                            <h4 class="alert alert-info" role="alert">Now accepting sign ups for beta usage</h4>
-                            <h2>Create your free PartHub account</h2>
-                            @php
-                                if (isset($_GET['cnv'])) {
-                                    echo '<div class="alert alert-dark" role="alert">reCAPTCHA was not verified</div>';
-                                }
-                            @endphp
-                        {{-- </th>
-                    </tr>
-                </thead>
-                <tbody> --}}
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <tr>
-                            <td style='text-align:left'>
-                                {{-- User Name --}}
-                                <label for="name" class="form-label">User Name</label>
-                                <input class="form-control" id="name" type="text" name="name" required autofocus
-                                    autocomplete="name">
-                                <x-input-error :messages="$errors->get('name')" />
+        <div class="col bg-light pt-2 rounded">
 
-                                {{-- Email Address --}}
-                                <label for="email" class="form-label">Email</label>
-                                <input id="email" type="email" class="form-control" type="email" name="email"
-                                    required autocomplete="username">
-                                <x-input-error :messages="$errors->get('email')" />
+            <h4 class="alert alert-info mt-1 text-center" role="alert">Now accepting sign ups for beta usage</h4>
+            <div class="text-center mb-3">
+                <h2>Create your free account</h2>
+            </div>
 
-                                {{-- Password --}}
-                                <label for="password" class="form-label">Password</label>
-                                <input id="password" type="password" name="password" required autocomplete="new-password"
-                                    class="form-control">
-                                <x-input-error :messages="$errors->get('password')" />
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                {{-- User Name --}}
+                <label for="name" class="form-label">User Name</label>
+                <input class="form-control" id="name" type="text" name="name" required autofocus
+                    autocomplete="name">
+                <x-input-error :messages="$errors->get('name')" />
 
-                                {{-- Confirm Password --}}
-                                <label for="password_confirmation" class="form-label">Confirm Password</label>
-                                <input id="password_confirmation" type="password" name="password_confirmation" required
-                                    autocomplete="new-password" class="form-control">
-                                <x-input-error :messages="$errors->get('password_confirmation')" />
+                {{-- Email Address --}}
+                <label for="email" class="form-label">Email</label>
+                <input id="email" type="email" class="form-control" type="email" name="email" required
+                    autocomplete="username">
+                <x-input-error :messages="$errors->get('email')" />
 
-                                <br>
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" required>
-                                    <label class="form-check-label" for="flexSwitchCheckDefault">I agree to the <a
-                                            href="{{ route('TOS') }}" target="_blank">Terms of Service</a></label>
-                                </div>
+                {{-- Password --}}
+                <label for="password" class="form-label">Password</label>
+                <input id="password" type="password" name="password" required autocomplete="new-password"
+                    class="form-control">
+                <x-input-error :messages="$errors->get('password')" />
 
+                {{-- Confirm Password --}}
+                <label for="password_confirmation" class="form-label">Confirm Password</label>
+                <input id="password_confirmation" type="password" name="password_confirmation" required
+                    autocomplete="new-password" class="form-control">
+                <x-input-error :messages="$errors->get('password_confirmation')" />
 
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <button type="submit" class="btn btn-primary" id="signupBtn" disabled>Sign up</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="text-align:center">
-                                <p class="fw-light">We don't tend lightly to bots around here</p>
-                                <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha.siteKey') }}"
-                                    data-callback="processChallenge">
-                                    <x-input-error :messages="$errors->get('recaptcha')" class="mt-2" />
-                                </div>
-                                <input type="hidden" id="recaptchaResponse" name="recaptcha_response">
-                            {{-- </td>
-                        </tr>
-                    </form>
-                </tbody>
-            </table> --}}
+                <br>
+                <div class="form-check form-switch mb-3">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" required>
+                    <label class="form-check-label" for="flexSwitchCheckDefault">I agree to the <a
+                            href="{{ route('TOS') }}" target="_blank">Terms of Service</a></label>
+                </div>
+                <div class="my-2 text-center">
+                    <button type="submit" class="btn btn-lg btn-primary" id="signupBtn" disabled>Create account</button>
+                </div>
+
+                <div class="text-center mt-3">
+                    <p class="fw-light">We don't tend lightly to bots around here</p>
+                    <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha.siteKey') }}"
+                        data-callback="processChallenge">
+                        <x-input-error :messages="$errors->get('recaptcha')" class="mt-2" />
+                    </div>
+                    <input type="hidden" id="recaptchaResponse" name="recaptcha_response">
+                </div>
         </div>
-        {{-- <div class="col-3"></div> --}}
     </div>
 @endsection
 
--- resources/views/whatis.blade.php
+++ resources/views/whatis.blade.php
@@ -4,12 +4,13 @@
 @section('content')
     <div class="container my-5">
         <!-- Hero Section -->
-        <div class="row justify-content-center text-center hero-section rounded-3 mb-5" style="background-image: url('app-related/hero-background.webp'); background-size: cover; background-position: center;">
+        <div class="row justify-content-center text-center hero-section rounded-3 mb-5"
+            style="background-image: url('app-related/hero-background.webp'); background-size: cover; background-position: center;">
             <div class="col-md-8">
                 <h1 class="display-1 mb-4">Welcome to PartHub</h1>
                 <p class="lead mb-4 bg-dark text-white p-2 rounded d-inline-block p-hero">Your Ultimate Inventory and BOM
                     Management Solution</p><br>
-                <a href="#features" class="btn btn-lg btn-light">Explore Demo</a>
+                <a href="{{ route('demo.login') }}" class="btn btn-lg btn-light me-2">Explore Demo</a>
                 <a href="{{ route('signup') }}" class="btn btn-lg btn-light ml-2">Sign Up Now</a>
             </div>
         </div>
@@ -19,10 +20,13 @@
             <div class="col-md-8">
                 <div class="card shadow-sm rounded">
                     <div class="card-body text-center">
+                        {{-- <div class="card-body text-center bg-primary bg-gradient text-white"> --}}
+
                         <h2 class="display-4 mb-4">Start with a clean slate</h2>
                         <img src="app-related/image1.webp" class="img-fluid mb-4"
                             alt="Person organizing parts in a workshop">
-                        <p class="lead">You work best in a tidy environment. PartHub is designed to let you focus on the creative
+                        <p class="lead">You work best in a tidy environment. PartHub is designed to let you focus on the
+                            creative
                             parts of your work. It is an Inventory and BOM management software.</p>
                     </div>
                 </div>
@@ -33,7 +37,7 @@
         <div class="row justify-content-center mb-5">
             <div class="col-md-8">
                 <div class="card shadow-sm rounded">
-                    <div class="card-body text-center bg-gradient text-white" style="background-color: #6c757d;">
+                    <div class="card-body text-center bg-primary bg-gradient text-white rounded">
                         <h2 class="display-4 mb-4">Work better</h2>
                         <img src="app-related/image2.webp" class="img-fluid mb-4" alt="Person using a barcode scanner">
                         <p class="lead">Track stock like a pro. It's fun and efficient, with or without a barcode scanner.
@@ -49,10 +53,32 @@
             <div class="col-md-8">
                 <div class="card shadow-sm rounded">
                     <div class="card-body text-center">
-                        <h2 class="display-4 mb-4">Is PartHub For You?</h2>
+                        <h2 class="display-4 mb-4">Creativity starts here</h2>
                         <img src="app-related/image3.webp" class="img-fluid mb-4" alt="Close-up of electronic components">
                         <p class="lead">If you handle small parts for your projects, PartHub is here to simplify your
                             life. Ideal for electronic parts but customizable for any type of parts.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Features -->
+        <div class="row justify-content-center mb-5" id="features">
+            <div class="col-md-8">
+                <div class="card-body text-center bg-primary bg-gradient text-white rounded">
+                    <div class="card-body text-center">
+                        <h2 class="display-4 mb-4">Features</h2>
+                        {{-- <ul class="list-unstyled lead"> --}}
+                            <ul class="list-group lead text-start">
+                            <li class="list-group-item active">📦 Keep an up-to-date inventory easily</li>
+                            <li class="list-group-item active">🏷️ Manage multiple storage locations</li>
+                            <li class="list-group-item active">📋 Handle suppliers, footprints, units, and BOMs</li>
+                            <li class="list-group-item active">📝 Automatic part history tracking</li>
+                            <li class="list-group-item active">⚙️ Assemble BOMs with automatic stock deduction</li>
+                            <li class="list-group-item active">🔔 Set notification thresholds</li>
+                            <li class="list-group-item active">📷 Upload images for each part</li>
+                        </ul>
+                        <a href="{{ route('demo.login') }}" class="btn btn-lg btn-outline-light ml-2 cta-btn mt-5">Try It For Yourself</a>
                     </div>
                 </div>
             </div>
@@ -64,35 +90,14 @@
                 <div class="card shadow-sm rounded">
                     <div class="card-body text-center bg-gradient text-white" style="background-color: #6c757d;">
                         <h2 class="display-4 mb-4">Who is Behind PartHub?</h2>
-                        <p class="lead">Created by <a href="https://christianzollner.com" target="_blank"
+                        <p class="lead">PartHub was created by <a href="https://christianzollner.com" target="_blank"
                                 class="text-white font-weight-bold">Christian Zollner</a>, founder of <a
                                 href="https://koma-elektronik.com" target="_blank" class="text-white font-weight-bold">KOMA
                                 Elektronik</a>. Christian programmed this software because no inventory tracking software of
                             this kind existed for his company.</p>
                         <img src="app-related/Chrisi_und_Kaja_square.webp" class="img-fluid rounded-circle w-25 mb-3"
                             alt="Christian and his dog Kaja">
-                        <p class="lead">Christian and his dog Kaja</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Features -->
-        <div class="row justify-content-center mb-5" id="features">
-            <div class="col-md-8">
-                <div class="card shadow-sm border-light rounded">
-                    <div class="card-body text-center">
-                        <h2 class="display-4 mb-4">Features</h2>
-                        <ul class="list-unstyled lead">
-                            <li>📦 Keep an up-to-date inventory easily</li>
-                            <li>🏷️ Manage multiple storage locations</li>
-                            <li>📋 Handle suppliers, footprints, units, and BOMs</li>
-                            <li>📝 Automatic part history tracking</li>
-                            <li>⚙️ Assemble BOMs with automatic stock deduction</li>
-                            <li>🔔 Set notification thresholds</li>
-                            <li>📷 Upload images for each part</li>
-                        </ul>
-                        <a href="{{ route('signup') }}" class="btn btn-lg btn-primary mt-3">Get Started Now</a>
+                        <p class="lead fw-lighter">Christian and his dog Kaja</p>
                     </div>
                 </div>
             </div>
@@ -127,7 +132,7 @@
                     <div class="card-body text-center bg-primary bg-gradient text-white">
                         <h2 class="display-4 mb-4">Ready to Get Started?</h2>
                         <p class="lead">Sign up today and start organizing your parts like never before!</p>
-                        <a href="{{ route('signup') }}" class="btn btn-lg btn-light cta-btn">Sign Up Now</a>
+                        <a href="{{ route('signup') }}" class="btn btn-lg btn-light cta-btn me-3">Sign Up Now</a>
                         <a href="{{ route('demo.login') }}" class="btn btn-lg btn-outline-light ml-2 cta-btn">Try Demo</a>
                     </div>
                 </div>
