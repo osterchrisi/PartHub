79e6deaff797c3b0d0d58b4238baf3dfec5c79a9
chrisi <chrisi@mobstationT.local>
2024-08-13 17:50:09 +0200

    Redirect back to login after logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -51,6 +51,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 }
