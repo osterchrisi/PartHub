b4df67e1c05ee510c782d75518b3dfa6e2e72b48
chrisi <chrisi@mobstationT.local>
2024-08-24 21:03:57 +0200

    Correct What is PartHub link showing

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -8,39 +8,56 @@ $user_name = $user ? $user->name : '';
     <nav class="navbar navbar-expand-lg bg-gradient">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">PartHub</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('parts') ? 'active' : '' }}" href="{{ route('parts') }}">Parts</a>
+                        <a class="nav-link {{ request()->routeIs('parts') ? 'active' : '' }}"
+                            href="{{ route('parts') }}">Parts</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('boms') ? 'active' : '' }}" href="{{ route('boms') }}">BOMs</a>
+                        <a class="nav-link {{ request()->routeIs('boms') ? 'active' : '' }}"
+                            href="{{ route('boms') }}">BOMs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('locations') ? 'active' : '' }}" href="{{ route('locations') }}">Storage Locations</a>
+                        <a class="nav-link {{ request()->routeIs('locations') ? 'active' : '' }}"
+                            href="{{ route('locations') }}">Storage Locations</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('suppliers') ? 'active' : '' }}" href="{{ route('suppliers') }}">Suppliers</a>
+                        <a class="nav-link {{ request()->routeIs('suppliers') ? 'active' : '' }}"
+                            href="{{ route('suppliers') }}">Suppliers</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ request()->routeIs('footprints') ? 'active' : '' }}" href="{{ route('footprints') }}">Footprints</a>
+                        <a class="nav-link {{ request()->routeIs('footprints') ? 'active' : '' }}"
+                            href="{{ route('footprints') }}">Footprints</a>
                     </li>
-                    @subscribed('maker')
-                    maker
-                    @endsubscribed
                 </ul>
                 <div class="d-flex">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li>
-                            <a class="nav-link {{ request()->routeIs('whatis') ? 'active' : '' }}" href="{{ route('whatis') }}">What is PartHub?</a>
-                        </li>
+                        @unless (Auth::check())
+                            {{-- Show "What is PartHub?" link for non-logged-in users --}}
+                            <li>
+                                <a class="nav-link {{ request()->routeIs('whatis') ? 'active' : '' }}"
+                                    href="{{ route('whatis') }}">What is PartHub?</a>
+                            </li>
+                        @endunless
+
+                        @if ($user_id == 1)
+                            {{-- Show "What is PartHub?" link for demo user --}}
+                            <li>
+                                <a class="nav-link {{ request()->routeIs('whatis') ? 'active' : '' }}"
+                                    href="{{ route('whatis') }}">What is PartHub?</a>
+                            </li>
+                        @endif
+
                         @unless (Auth::check())
                             {{-- Show Sign Up Link / Demo button if not authorized (not signed in) --}}
                             <li class="me-2 mb-2">
-                                <a class="btn btn-warning {{ request()->routeIs('signup') ? 'active' : '' }}" href="{{ route('signup') }}" id="continueDemoLink">Create Account</a>
+                                <a class="btn btn-warning {{ request()->routeIs('signup') ? 'active' : '' }}"
+                                    href="{{ route('signup') }}" id="continueDemoLink">Create Account</a>
                             </li>
                             <li>
                                 <form id="demoLoginButton" action="{{ route('demo.login') }}" method="GET">
@@ -49,14 +66,17 @@ $user_name = $user ? $user->name : '';
                                 </form>
                             </li>
                         @endunless
+
                         @if ($user_id == 1)
-                            {{-- Show Sign Up Link if demo user (signed in) --}}
+                            {{-- Show Sign Up Link if demo user --}}
                             <li>
-                                <a class="btn btn-warning {{ request()->routeIs('signup') ? 'active' : '' }}" href="{{ route('signup') }}" id="continueDemoLink">Create Account</a>
+                                <a class="btn btn-warning {{ request()->routeIs('signup') ? 'active' : '' }}"
+                                    href="{{ route('signup') }}" id="continueDemoLink">Create Account</a>
                             </li>
                         @endif
                         <li>
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"><i class="fas fa-user"></i></a>
+                            <a class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown"><i class="fas fa-user"></i></a>
                             <ul class="dropdown-menu dropdown-menu-end text-end w-auto px-2 navbar-dropdown-colored">
                                 @if (Auth::check())
                                     <!-- Logged in -->
@@ -87,4 +107,3 @@ $user_name = $user ? $user->name : '';
         </div>
     </nav>
 </div>
-
