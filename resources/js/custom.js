86bed6139352cb03539c2125b132bc3e2d9c9234
chrisi <chrisi@mobstationT.local>
2024-09-03 23:32:02 +0200

    UI

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -336,7 +336,6 @@ export function updateInfoWindow(type, id) {
 export function clearModalOnHiding() {
     $(document).on('hidden.bs.modal', '.modal', function (event) {
         // Don't do it if the modal was hidden due to creating a new category
-        console.log("hiding any modal");
         const targetModalId = event.target.id;
         if (targetModalId !== 'categoryCreationModal') {
             // Check if part entry modal is being hidden due to showing category modal
--- resources/views/components/upload-form.blade.php
+++ resources/views/components/upload-form.blade.php
@@ -9,17 +9,24 @@
                 <div class="card-body">
                     <form method="POST" enctype="multipart/form-data" id="{{ $formId }}">
                         @csrf
-                        <div class="mb-3">
+                        <div class="input-group mb-3">
                             {{-- <label for="{{ $inputId }}" class="form-label form-label-sm">{{ $labelText }}</label> --}}
-                            <input type="file" class="form-control" id="{{ $inputId }}" name="{{ $inputName }}" accept="{{ $acceptType }}" required>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <button type="submit" class="btn btn-sm btn-primary">{{ $buttonText }}</button>
+                            <input type="file" class="form-control form-control-sm" id="{{ $inputId }}"
+                                name="{{ $inputName }}" accept="{{ $acceptType }}" required>
+                            <button type="submit" class="btn btn-outline-primary btn-sm">{{ $buttonText }}</button>
                             <div id="{{ $loadingId }}" class="ms-2" style="display: none;">
                                 <div class="spinner-border text-primary" role="status">
                                     <span class="visually-hidden">Loading...</span>
                                 </div>
                             </div>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            {{-- <button type="submit" class="btn btn-sm btn-primary">{{ $buttonText }}</button>
+                            <div id="{{ $loadingId }}" class="ms-2" style="display: none;">
+                                <div class="spinner-border text-primary" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
+                            </div> --}}
                         </div>
                     </form>
                 </div>
