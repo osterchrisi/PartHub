b5aa33510dcc7a0fab2c7831a3db94f38e106cc7
chrisi <chrisi@mobstationT.local>
2024-09-17 18:26:42 +0200

    Supplier Data migration command

--- app/Console/Commands/MigrateSuppliersToSupplierData.php
+++ app/Console/Commands/MigrateSuppliersToSupplierData.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Part;
 use App\Models\SupplierData;
-use Illuminate\Support\Facades\Auth;
 
 class MigrateSuppliersToSupplierData extends Command
 {
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        // Commands\MigrateSuppliersToSupplierData::class,
+    ];
+    
     /**
      * Define the application's command schedule.
      */
