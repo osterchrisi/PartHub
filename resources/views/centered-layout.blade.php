e40471ccff4a4d56a67d7b8490bc250b302db057
chrisi <chrisi@mobstationT.local>
2024-09-27 20:37:38 +0200

    Welcome screen

--- resources/views/centered-layout.blade.php
+++ resources/views/centered-layout.blade.php
@@ -2,7 +2,7 @@
 @include('navbar')
 
 <div
-    class="d-flex flex-grow-1 justify-content-center align-items-center {{ isset($view) && in_array($view, ['whatis', 'signup', 'verify-mail', 'Forgot Password', 'register-testing']) ? 'landing-page' : '' }}">
+    class="d-flex flex-grow-1 justify-content-center align-items-center {{ isset($view) && in_array($view, ['whatis', 'signup', 'verify-mail', 'Forgot Password', 'register-testing', 'welcome']) ? 'landing-page' : '' }}">
     <div class="greeting d-flex align-items-center">
         @yield('content')
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,68 +16,58 @@
                         further instructions.
                         🚀</div>
                 @endif
-
-                @unless (auth()->check())
-                    Hello internet stranger that has found their way to PartHub! 👋 <br>
-                    PartHub is currently in beta, you can use it and snatch a <a href="{{ route('signup') }}">free early-bird
-                        account</a>.<br><br>
-
-                    If you just want to hang around and explore the app, click some stuff, that is fine with us too!<br>
-                    Go ahead and explore by trying the demo!<br>
-                    (As a heads up, the demo database will reset every once in a while.)<br><br>
-                @endunless
             </td>
         </tr>
         <tr>
-            <td width="33.3%"><a href="{{ route('parts') }}">
-                    <h1><i class="bi bi-cpu"></i></h1>Parts<br><br>
-                </a></td>
-            <td width="33.3%"><a href="{{ route('boms') }}">
-                    <h1><i class="bi bi-clipboard-check"></i></h1>BOMs<br>
-                </a></td>
-            <td width="33.3%"><a href="{{ route('locations') }}">
-                    <h1><i class="bi bi-buildings"></i></h1>Storage<br>
-                </a></td>
+            <td width="33.3%">
+                <div class="list-group">
+                    <a href="{{ route('parts') }}" class="list-group-item list-group-item-action text-center bg-transparent welcome-items">
+                        <h1><i class="bi bi-cpu"></i></h1>Parts
+                    </a>
+                </div>
+            </td>
+            <td width="33.3%">
+                <div class="list-group">
+                    <a href="{{ route('boms') }}" class="list-group-item list-group-item-action text-center bg-transparent welcome-items">
+                        <h1><i class="bi bi-clipboard-check"></i></h1>BOMs
+                    </a>
+                </div>
+            </td>
+            <td width="33.3%">
+                <div class="list-group">
+                    <a href="{{ route('locations') }}" class="list-group-item list-group-item-action text-center bg-transparent welcome-items">
+                        <h1><i class="bi bi-buildings"></i></h1>Storage
+                    </a>
+                </div>
+            </td>
         </tr>
         <tr>
             <td colspan="3">
                 <table class="table table-borderless">
                     <tr>
-                        <td width="18%"></td>
-                        <td width="25%"><a href="{{ route('suppliers') }}">
-                                <h1><i class="bi bi-cart2"></i></h1>Suppliers
-                            </a></td>
-                        <td width="14%"></td>
-                        <td width="25%"><a href="{{ route('footprints') }}">
-                                <h1><i class="bi bi-outlet"></i></h1>Footprints
-                            </a></td>
-                        <td width="18%"></td>
+                        <td width="11.33%"></td>
+                        <td width="33%">
+                            <div class="list-group">
+                                <a href="{{ route('suppliers') }}" class="list-group-item list-group-item-action text-center bg-transparent welcome-items">
+                                    <h1><i class="bi bi-cart2"></i></h1>Suppliers
+                                </a>
+                            </div>
+                        </td>
+                        <td width="11.33%"></td>
+                        <td width="33%">
+                            <div class="list-group">
+                                <a href="{{ route('footprints') }}" class="list-group-item list-group-item-action text-center bg-transparent welcome-items">
+                                    <h1><i class="bi bi-outlet"></i></h1>Footprints
+                                </a>
+                            </div>
+                        </td>
+                        <td width="11.33%"></td>
                     </tr>
                 </table>
             </td>
         </tr>
-        {{-- @unless (auth()->check())
-            <tr>
-                <td colspan="3">
-                    <table class="table table-borderless">
-                        <tbody class="alert alert-danger">
-                            <tr>
-                                <td>
-                                    <form id="demoLoginButton" action="{{ route('demo.login') }}" method="GET">
-                                        @csrf<button type="submit" class="btn btn-primary" id="continueDemo">Continue as demo
-                                            user</button></form>
-                                </td>
-                                <td>
-                                    <form id="login-button" action="{{ route('login') }}" method="GET">
-                                        @csrf<button type="submit" class="btn btn-primary" id="logIn">Log into your
-                                            account</button></form>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
-        @endunless --}}
+        
+        
     </table>
     <!-- Google tag (gtag.js) event -->
     <script>
