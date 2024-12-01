7e064c578d46ff788f18517eaa69b29c7c26821e
chrisi <chrisi@mobstationT.local>
2024-12-01 01:30:42 +0100

    Imprint

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -5,11 +5,12 @@
     <table class="table table-borderless text-center mx-auto w-auto" style="borders: false">
         <tr>
             <td>
-                PartHub<br>
-                Koloniestr. 29<br>
+                <span class="fw-medium">PartHub</span><br>
+                <span class="fw-normal">Koloniestr. 29</span><br>
                 13359 Berlin<br>
-                Germany<br>
-                Court of Registration: Amtsgericht Mitte
+                Germany<br><br>
+                Court of Registration: Amtsgericht Mitte<br>
+                Tax ID: 23/608/01230
             </td>
         </tr>
     </table>
