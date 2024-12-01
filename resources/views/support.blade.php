f40e3152f9e40eeb415d1494ea64dc15aba84409
chrisi <chrisi@mobstationT.local>
2024-12-01 01:25:46 +0100

    Support

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -2,18 +2,25 @@
 @extends('centered-layout')
 
 @section('content')
-    <table class="table table-borderless text-center mx-auto w-auto" style="borders: false">
-        <tr>
-            <td>
-                <h1>Questions?</h1>
-            </td>
-        </tr>
-        <tr>
-            <td>
-               Drop us a line at: <span id="email"></span>
-            </td>
-        </tr>
-    </table>
+    <div class="container text-center py-5">
+        <div class="row">
+            <div class="col">
+                <h1 class="display-4 mb-4">
+                    Questions?
+                </h1>
+                <p class="lead mb-5">
+                    We're here to help. Feel free to reach out anytime!
+                </p>
+                <div>
+                    <span class="fw-medium">
+                        Drop us a line at:
+                    </span>
+                    <br>
+                    <span id="email" class="mt-2 fs-4 fw-bold text-primary"></span>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('modals and menus')
