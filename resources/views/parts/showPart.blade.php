ea2b4a8878a7e202393dc449fd4f746344e46eb7
chrisi <chrisi@mobstationT.local>
2025-01-26 20:52:09 +0100

    Add description, comment to showPart Blade

--- resources/views/parts/showPart.blade.php
+++ resources/views/parts/showPart.blade.php
@@ -3,6 +3,7 @@
     <div class="row justify-content-between pe-3">
         <div class="col">
             <h4>{{ $part['part_name'] }}</h4>
+            <h6 class="text-muted">{{ $part['part_description'] }}</h6>
             <h5>Total stock:
                 {{ $total_stock }}
             </h5>
@@ -16,9 +17,12 @@
 
     <!-- Tabs -->
     <x-tablist id="partTabs" defaultTab="{{ $tabId1 }}">
-        <x-buttons.tab-button id="{{ $tabId1 }}" toggleTarget="{{ $tabToggleId1 }}" tabText="{{ $tabText1 }}" />
-        <x-buttons.tab-button id="{{ $tabId2 }}" toggleTarget="{{ $tabToggleId2 }}" tabText="{{ $tabText2 }}" />
-        <x-buttons.tab-button id="{{ $tabId3 }}" toggleTarget="{{ $tabToggleId3 }}" tabText="{{ $tabText3 }}" />
+        <x-buttons.tab-button id="{{ $tabId1 }}" toggleTarget="{{ $tabToggleId1 }}"
+            tabText="{{ $tabText1 }}" />
+        <x-buttons.tab-button id="{{ $tabId2 }}" toggleTarget="{{ $tabToggleId2 }}"
+            tabText="{{ $tabText2 }}" />
+        <x-buttons.tab-button id="{{ $tabId3 }}" toggleTarget="{{ $tabToggleId3 }}"
+            tabText="{{ $tabText3 }}" />
     </x-tablist>
 
     <!-- Tabs Content -->
@@ -43,4 +47,24 @@
 
         </div>
     </div>
+    <div class="pb-3 pe-3 pt-3">
+        <h6>Comment</h6>
+        {{-- <p class="form-control bg-light">{{ $part['part_comment'] }}</p> --}}
+        <table>
+            <tr>
+                <td data-editable="true" class="editable editable-text" data-id="{{ $part['part_id'] }}"
+                    data-column="part_comment" data-table_name="parts" data-id_field="part_id">
+                    <x-tables.td-editable-flexbox :content="$part['part_comment'] ?? ''">
+                        {{-- Not yet working
+                                <x-tables.copy-clipboard :content="$part[$column_data] ?? ''" /> --}}
+                        <x-tables.edit-pen />
+                    </x-tables.td-editable-flexbox>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+
+
+
 </div>
